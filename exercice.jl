bb28714e212f1d9acbad3fbff8ccc7cf3aa3c847
Dominique Orban <dominique.orban@gmail.com>
2025-05-07 16:49:10 -0400

    initial commit

---
+++ exercice.jl
@@ -0,0 +1,19 @@
+using LinearAlgebra
+
+n = 3
+# Supposons que g = ∇f(x)
+g = ones(n)
+# Supposons que H = ∇²f(x)
+H = [6.0 2.0 3.0;
+     2.0 7.0 3.5
+     3.0 3.5 8.0]
+
+# Sur base de H et g, calculer la direction de Newton d
+
+### votre code ici ; ne rien modifier d'autre
+d = rand(n)
+###
+
+# vérification
+using Test
+@test norm(H * d + g) ≤ sqrt(eps()) * norm(g)
